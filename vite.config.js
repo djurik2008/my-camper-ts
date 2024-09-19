9c0e976436ba231d44f32c2924f874732e148a07
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-09-19 11:00:37 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      modules: '/src/modules',
+      hooks: '/src/hooks',
+      pages: '/src/pages',
+      '@redux': '/src/redux',
+      shared: '/src/shared',
+      helpers: '/src/helpers',
+      context: '/src/context',
+      services: '/src/services',
+    },
+  },
+  base: '/my-camper',
+});
