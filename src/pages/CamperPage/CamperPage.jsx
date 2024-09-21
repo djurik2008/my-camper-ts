21df6dce34233757f036cff1e130115320092909
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-09-21 15:30:42 +0300

    add starting files for camperPage, add routes to App.jsx

---
+++ src/pages/CamperPage/CamperPage.jsx
@@ -0,0 +1,5 @@
+const CamperPage = () => {
+  return <h1>Camper details</h1>;
+};
+
+export default CamperPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ import { lazy } from 'react';
 export const HomePage = lazy(() => import('./HomePage'));
 export const CatalogPage = lazy(() => import('./CatalogPage'));
 export const NotFoundPage = lazy(() => import('./NotFoundPage'));
+export const CamperPage = lazy(() => import('./CamperPage'));
