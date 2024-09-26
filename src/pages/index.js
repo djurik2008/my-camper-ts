c4f852d964f7adb214a81cbf89c895b1a0558292
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-09-26 15:32:09 +0300

    add BrowserRouter to main.jsx, change imports in pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { lazy } from 'react';
 
-export const HomePage = lazy(() => import('./HomePage'));
-export const CatalogPage = lazy(() => import('./CatalogPage'));
-export const NotFoundPage = lazy(() => import('./NotFoundPage'));
-export const CamperPage = lazy(() => import('./CamperPage'));
+export const HomePage = lazy(() => import('./HomePage/HomePage'));
+export const CatalogPage = lazy(() => import('./CatalogPage/CatalogPage'));
+export const NotFoundPage = lazy(() => import('./NotFoundPage/NotFoundPage'));
+export const CamperPage = lazy(() => import('./CamperPage/CamperPage'));
